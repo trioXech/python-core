b11f103dd27045a4af19fe8e478d15473cf6a5a7
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-07-10 18:08:26 +0600

    Basic Problems Solutions added

---
+++ 1.Basic/1.1.Numbers/Problems/Solutions/mehedi-durjoi/pythonDivision.py
@@ -0,0 +1,6 @@
+# taking input and convert those in integer type
+a = int(input())
+b = int(input())
+
+print(a//b) # integer division
+print(a/b)  # float division
---
+++ 2.MoreControlFlowTools/2.6.DefiningFunctions/Problems/Solutions/mehedi-durjoi/printFunction.py
@@ -0,0 +1,4 @@
+n = int(input()) # Taking integer input
+
+print(*range(1, n+1), sep='')
+# Printing all the values from 1 to n separeted by ''
